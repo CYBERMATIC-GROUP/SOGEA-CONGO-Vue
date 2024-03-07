91179114eb3f7441b1604f9fcba0c2dcac02053e
Rosly <rosly.mokambo@ginov.io>
2024-03-07 19:06:45 +0100

    Initialisation du projet avec Vue js

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="w-full h-screen items-center flex justify-center">
+    <h1 class="font-[600] text-5xl text-blue-500">Sogea Congo</h1>
+  </main>
+</template>
